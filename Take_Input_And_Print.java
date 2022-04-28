7eb832b347eb4e980bef61fc30a156c7728841a6
IllaSandeep <96726447+IllaSandeep@users.noreply.github.com>
2022-04-29 01:59:21 +0530

    import java.util.Scanner; class solution {     public static void main(String args[])     {         Scanner scan=new Scanner(System.in);         String str=scan.nextLine();         System.out.print(str);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class solution {
+    public static void main(String args[])
+    {
+        Scanner scan=new Scanner(System.in);
+        String str=scan.nextLine();
+        System.out.print(str);
+    }
+}
