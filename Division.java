04064f17df2ba6e0815f66877ee4a9a110174316
IllaSandeep <96726447+IllaSandeep@users.noreply.github.com>
2022-04-29 01:47:46 +0530

    import java.util.Scanner; class solution {     public static void main(String args[])     {         Scanner scan=new Scanner(System.in);         int a=scan.nextInt();         int b=scan.nextInt();         scan.close();         int p=a/b;         System.out.println(p);     } }

---
+++ Division.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class solution {
+    public static void main(String args[])
+    {
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        scan.close();
+        int p=a/b;
+        System.out.println(p);
+    }
+}
