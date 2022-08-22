b7abdc46339ab72d4ec418c643a0a3478a025457
Sandeep <96726447+IllaSandeep@users.noreply.github.com>
2022-08-22 23:35:03 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         int[] a=new int[n];         int s=0;         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             s+=a[i];         }         float avg=(float)s/n;         System.out.format("%.2f",avg);
        }
    }

---
+++ average_of_the_array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        int[] a=new int[n];
+        int s=0;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            s+=a[i];
+        }
+        float avg=(float)s/n;
+        System.out.format("%.2f",avg);
+        
+    }
+}
