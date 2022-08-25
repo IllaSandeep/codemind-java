f3afe360628d3199f1eeff7565dbe28a26d8e5e2
Sandeep <96726447+IllaSandeep@users.noreply.github.com>
2022-08-25 22:11:37 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         Set<Integer> l=new HashSet<Integer>();         List<Integer>k=new ArrayList<Integer>();         int n=sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         Arrays.sort(a);         for(int i=0;i<n;i++)         {             for(int j=0;j<n;j++)             {                 if(a[i]==a[j] && i!=j)                 {                     a[j]=-1;                 }             }         }         for(int i=0;i<n;i++)         {             if(a[i]!=-1)             {                k.add(a[i]);             }         }
             if(k.size()>=3)
             {
             System.out.println(k.get(k.size()-3));
             }
             else if(k.size()==2)
             {
                 System.out.println(k.get(k.size()-1));
             }
             else if(k.size()==1)
             {
                 System.out.println(k.get(k.size()-1));
             }
        }
    }

---
+++ _Third_Maximum_Number.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        Set<Integer> l=new HashSet<Integer>();
+        List<Integer>k=new ArrayList<Integer>();
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        Arrays.sort(a);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(a[i]==a[j] && i!=j)
+                {
+                    a[j]=-1;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]!=-1)
+            {
+               k.add(a[i]);
+            }
+        }
+      
+         if(k.size()>=3)
+         {
+         System.out.println(k.get(k.size()-3));
+         }
+         else if(k.size()==2)
+         {
+             System.out.println(k.get(k.size()-1));
+         }
+         else if(k.size()==1)
+         {
+             System.out.println(k.get(k.size()-1));
+         }
+    }
+}
