2c53ee8d320b3d9de0bb9507c63a22600ec82f38
Sandeep <96726447+IllaSandeep@users.noreply.github.com>
2022-08-27 13:42:04 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         int sum=0;         for(int i=0;i<s.length();i++)         {             if(s.charAt(i)>='0' && s.charAt(i)<='9')             {                 sum+=s.charAt(i)-'0';             }         }         System.out.println(sum);     } }

---
+++ Numbers_in_String.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int sum=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)>='0' && s.charAt(i)<='9')
+            {
+                sum+=s.charAt(i)-'0';
+            }
+        }
+        System.out.println(sum);
+    }
+}
