9fe65d3b7a1fe0c2ce78fb288fbd443baeb99f29
Sandeep <96726447+IllaSandeep@users.noreply.github.com>
2022-08-30 00:29:12 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         String n="";         for(int i=s.length()-1;i>=0;i--)         {             n+=s.charAt(i);         }         System.out.println(n);     } }

--- Reverse_String.java
+++ Reverse_String.java
@@ -4,12 +4,12 @@ class Solution
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        String s[]=sc.nextLine().split(" ");
-        String ans = "";
-        for (int i=s.length-1;i>=0;i--)
+        String s=sc.nextLine();
+        String n="";
+        for(int i=s.length()-1;i>=0;i--)
         {
-            ans+=s[i]+" ";
+            n+=s.charAt(i);
         }
-        System.out.println(ans.substring(0,ans.length()-1));
+        System.out.println(n);
     }
 }
