75ee2f1997c7296380ab135323951e55bdce95f4
Sandeep <96726447+IllaSandeep@users.noreply.github.com>
2022-08-29 17:50:21 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         String s=sc.next();         if(s.contains("HH"))         {             System.out.println("NO");         }         else         {             s=s.replace('.','B');             System.out.println("YES");             System.out.println(s);         }     } }

---
+++ Split_houses.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String s=sc.next();
+        if(s.contains("HH"))
+        {
+            System.out.println("NO");
+        }
+        else
+        {
+            s=s.replace('.','B');
+            System.out.println("YES");
+            System.out.println(s);
+        }
+    }
+}
