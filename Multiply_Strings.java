df0b86d6fe7ee9a49f55ae6e38dd1df1645d77ec
Sandeep <96726447+IllaSandeep@users.noreply.github.com>
2022-08-30 00:40:54 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         long s=sc.nextLong();         long s1=sc.nextLong();         System.out.println(s1*s);     } }

---
+++ Multiply_Strings.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        long s=sc.nextLong();
+        long s1=sc.nextLong();
+        System.out.println(s1*s);
+    }
+}
