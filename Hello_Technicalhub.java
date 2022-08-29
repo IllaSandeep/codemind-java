0310bc01df5404aab6ea1616c9082b8c3cc338e6
Sandeep <96726447+IllaSandeep@users.noreply.github.com>
2022-08-30 00:34:53 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         System.out.println("Hello Technicalhub");         System.out.println(s);
        }
    }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println("Hello Technicalhub");
+        System.out.println(s);
+        
+    }
+}
