fde033c7e9b8daa285db3f55bed9c44b6eb271cb
Sandeep <96726447+IllaSandeep@users.noreply.github.com>
2022-11-30 21:36:35 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int m=sc.nextInt();         int[][] a=new int[n][m];         for(int i=0;i<n;i++)         {             for(int j=0;j<m;j++)             {                 a[i][j]=sc.nextInt();             }         }         int c=0;         for(int i=0;i<n;i++)         {             for(int j=0;j<m;j++)             {                 if(a[i][j]<0)                 {                     c++;                 }             }         }         System.out.println(c);     } }

---
+++ Search_Negative_in_Grid.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int[][] a=new int[n][m];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                a[i][j]=sc.nextInt();
+            }
+        }
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(a[i][j]<0)
+                {
+                    c++;
+                }
+            }
+        }
+        System.out.println(c);
+    }
+}
